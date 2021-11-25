3edf345ac646e188fd93d3f385d9595d2dae6f38
Joao Victor Pereira <jonvicej@gmail.com>
2021-11-25 19:46:07 -0300

    Chave estrangeira nao nula (integridade referencial)

--- ProjetoIntegradoMVC/Migrations/20211125223941_DepartmentForeignKey.Designer.cs
+++ ProjetoIntegradoMVC/Migrations/20211125223941_DepartmentForeignKey.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoIntegradoMVC.Models;
 
 namespace ProjetoIntegradoMVC.Migrations
 {
     [DbContext(typeof(ProjetoIntegradoMVCContext))]
-    partial class ProjetoIntegradoMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20211125223941_DepartmentForeignKey")]
+    partial class DepartmentForeignKey
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
