17c135cbb03ce7b749a402d4c43c713d83269d8b
Joao Victor Pereira <jonvicej@gmail.com>
2021-11-25 04:21:59 -0300

    Outras entidades e segunda migration

--- ProjetoIntegradoMVC/Migrations/20211125071812_OtherEntities.Designer.cs
+++ ProjetoIntegradoMVC/Migrations/20211125071812_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoIntegradoMVC.Models;
 
 namespace ProjetoIntegradoMVC.Migrations
 {
     [DbContext(typeof(ProjetoIntegradoMVCContext))]
-    partial class ProjetoIntegradoMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20211125071812_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
