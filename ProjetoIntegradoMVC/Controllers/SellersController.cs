3bae65aaab28de9e8b1b9476f68324274982d6c5
Joao Victor Pereira <jonvicej@gmail.com>
2021-11-25 18:37:05 -0300

    Form Create simples

--- ProjetoIntegradoMVC/Controllers/SellersController.cs
+++ ProjetoIntegradoMVC/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjetoIntegradoMVC.Services;
+using ProjetoIntegradoMVC.Models;
 
 namespace ProjetoIntegradoMVC.Controllers
 {
@@ -20,6 +21,19 @@ namespace ProjetoIntegradoMVC.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
