5fda5d8d70e381443cdb11cdc20f87341ed4f105
Joao Victor Pereira <jonvicej@gmail.com>
2021-11-24 23:56:33 -0300

    CRUD Scaffolding

---
+++ ProjetoIntegradoMVC/Data/ProjetoIntegradoMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoIntegradoMVC.Models
+{
+    public class ProjetoIntegradoMVCContext : DbContext
+    {
+        public ProjetoIntegradoMVCContext (DbContextOptions<ProjetoIntegradoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjetoIntegradoMVC.Models.Department> Department { get; set; }
+    }
+}
