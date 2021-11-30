307a39911772a491be856eb0672d80a719e64cff
Joao Victor Pereira <jonvicej@gmail.com>
2021-11-30 17:21:26 -0300

    SellerFormViewModel e select do Department

---
+++ ProjetoIntegradoMVC/Services/DepartmentService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoIntegradoMVC.Models;
+
+namespace ProjetoIntegradoMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly ProjetoIntegradoMVCContext _context;
+
+        public DepartmentService(ProjetoIntegradoMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
