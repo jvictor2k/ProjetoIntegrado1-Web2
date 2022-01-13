9f580285721d6a02558443a0d73a9c94938b9379
Joao Victor Pereira <jonvicej@gmail.com>
2022-01-13 17:32:00 -0300

    Formatando pdf e nomeando tipos de dados para exibicao

--- ProjetoIntegradoMVC/Controllers/HomeController.cs
+++ ProjetoIntegradoMVC/Controllers/HomeController.cs
@@ -52,12 +52,23 @@ namespace ProjetoIntegradoMVC.Controllers
                 var graphics = PdfSharpCore.Drawing.XGraphics.FromPdfPage(page);
                 var corFonte = PdfSharpCore.Drawing.XBrushes.Black;
                 var textFormatter = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
-                var fonteSeller = new PdfSharpCore.Drawing.XFont("Arial", 10);
-                var fonteSales = new PdfSharpCore.Drawing.XFont("Arial", 8, PdfSharpCore.Drawing.XFontStyle.BoldItalic);
-                var fonteDepartment = new PdfSharpCore.Drawing.XFont("Arial", 8, PdfSharpCore.Drawing.XFontStyle.Bold);
+                var fonteSeller = new PdfSharpCore.Drawing.XFont("Arial", 12);
+                var fonteSales = new PdfSharpCore.Drawing.XFont("Arial", 12, PdfSharpCore.Drawing.XFontStyle.BoldItalic);
+                var fonteDepartment = new PdfSharpCore.Drawing.XFont("Arial", 12, PdfSharpCore.Drawing.XFontStyle.Bold);
 
                 var qtdPaginas = doc.PageCount;
                 textFormatter.DrawString(qtdPaginas.ToString(), new PdfSharpCore.Drawing.XFont("Arial", 10), corFonte, new PdfSharpCore.Drawing.XRect(578,825,page.Width,page.Height));
+
+                var alturaTituloDetalhesY = 40;
+                var detalhes = new PdfSharpCore.Drawing.Layout.XTextFormatter(graphics);
+
+                detalhes.DrawString("Seller", fonteSeller, corFonte, new PdfSharpCore.Drawing.XRect(30, alturaTituloDetalhesY, page.Width, page.Height));
+                detalhes.DrawString("Sales", fonteSales, corFonte, new PdfSharpCore.Drawing.XRect(150, alturaTituloDetalhesY, page.Width, page.Height));
+                detalhes.DrawString("Department", fonteDepartment, corFonte, new PdfSharpCore.Drawing.XRect(270, alturaTituloDetalhesY, page.Width, page.Height));
+
+                var alturaDetalhesItens = 60;
+
+
 
                 using (MemoryStream stream = new MemoryStream())
                 {
